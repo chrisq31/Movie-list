2f7979ddefa075f225c767339691d154d9c405c1
chrisquinncreative <quinn.uk@gmail.com>
2019-11-23 00:59:09 +0000

    design

--- src/js/components/GenresSubmitButton.js
+++ src/js/components/GenresSubmitButton.js
@@ -4,43 +4,42 @@ import clapperBoard from '../../img/clapperBoard.svg'
 
 
 const primaryFontFamily = "league_gothicregular";
+const bgColor = "#40142c";
+const textColor = "#ffffff";
+const textColorCTA = "#ffffff";
 
 const StyledButton = styled.div`
 
 display:flex;
 justify-content: center;
 align-items: center;
-  background-color: ${(props) => props.active ? "#000000" : "#40142c"};
-    color: ${(props) => props.active ? "#ffffff" : "#ffffff"};
-   border-color: "#ffffff";
-   text-align:center;
-    font-size: 5vw;
-  line-height:1em;
-  margin: 1em;
-  padding: .25em 1.5em;
- cursor:pointer;
-  border:10px;
-  border-radius: 80px 1px;
-  font-family: ${primaryFontFamily};
-  text-transform: uppercase;
-
-   overflow:hidden;
-
-   border:10px;
-   border-radius: 0px 0px -1% 0px;
-   box-shadow: -28px 18px 85px 32px rgba(245,7,7,0.75)
+background-color: ${bgColor};
+text-align:center;
+font-size: 5vw;
+line-height:1em;
+margin: 1em;
+padding: .25em 1.5em;
+cursor:pointer;
+border:10px;
+border-radius: 80px 1px;
+font-family: ${primaryFontFamily};
+text-transform: uppercase;
+overflow:hidden;
+border:10px;
+border-radius: 0px 0px -1% 0px;
+box-shadow: -28px 18px 85px 32px rgba(245,7,7,0.75)
 
 `;
 
 const Title = styled.h1`
   font-size: 1.5em;
   text-align: center;
-  color: palevioletred;
+  color: ${textColor};
 `;
 
-const TitleCta = styled(Title)`
-  color: tomato;
-  border-color: tomato;
+const TitleCTA = styled(Title)`
+  color: ${textColorCTA};
+ 
 `;
 
 
@@ -57,6 +56,7 @@ font-size: 8vw;
 background-image: url(${clapperBoard});
 background-repeat: no-repeat;
 background-size: contain;
+margin-left:30px;
 `
 
 
@@ -66,21 +66,16 @@ const ctaText = "GO!"
 const GenresSubmitButton = ({ onClick, isActive, genres }) => (
 
   <div className="row" onClick={onClick} >
-   
-    <StyledButton active={isActive}> 
-    <Title> {genres}</Title>
-   
 
-    <SubmitLogo>
-
-    <TitleCta> {ctaText}</TitleCta>
-      
-    </SubmitLogo>
-    
+    <StyledButton active={isActive}>
+      <Title> {genres}</Title>
+      <SubmitLogo>
+        <TitleCTA> {ctaText}</TitleCTA>
+      </SubmitLogo>
     </StyledButton>
-  
-    </div>
-   
+
+  </div>
+
 
 
 )
