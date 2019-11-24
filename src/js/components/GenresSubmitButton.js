c7b3c40bf6f2f46222b3f5e8073ab22dd61abdab
chrisquinncreative <quinn.uk@gmail.com>
2019-11-24 19:31:35 +0000

    cleanup

--- src/js/components/GenresSubmitButton.js
+++ src/js/components/GenresSubmitButton.js
@@ -19,7 +19,7 @@ margin: 0;
 padding: .25em 1.5em;
 cursor:pointer;
 
-border-radius: 70px 0px;
+
 font-family: ${primaryFontFamily};
 text-transform: uppercase;
 overflow:hidden;
@@ -31,7 +31,8 @@ box-shadow: -28px 18px 85px 32px rgba(245,7,7,0.75)
 
 const Title = styled.h1`
   font-size:calc(18px + 1vw);
-  text-align: center;
+  margin-right:5%;
+ 
   color: ${textColor};
   text-align:right;
 line-height:1em;
@@ -39,28 +40,24 @@ line-height:1em;
 
 const TitleCTA = styled(Title)`
   color: ${bgColor};
+  text-align: left;
   font-size:calc(24px + 1vw);
   margin-top:60%;
-  margin-left:26%;
+  margin-right:0;
  
 `;
 
 
-
-
-
-
-
 const SubmitLogo = styled.div`
-display:flex;
-width:100%;
+
 min-width:100px;
 height:inherit;
+display:block;
 
 background-image: url(${clapperBoard});
 background-repeat: no-repeat;
-background-size: cover;
-margin:1.6rem;
+
+
 
 `
 
