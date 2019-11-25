d611cefa26a7a1cfd8aa43e0eaa30219451d349a
chrisquinncreative <quinn.uk@gmail.com>
2019-11-25 07:49:59 +0000

    Updates

---
+++ precache-manifest.e9a832258d8eac205caad49fd0418514.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c2f205c8ecb35abf01bad717228bb0b",
+    "url": "/Movie-list/build/index.html"
+  },
+  {
+    "revision": "4f56efc30d98eada3ed0",
+    "url": "/Movie-list/build/static/css/main.284c1176.chunk.css"
+  },
+  {
+    "revision": "a5156c756a8b192966f5",
+    "url": "/Movie-list/build/static/js/2.58157a5c.chunk.js"
+  },
+  {
+    "revision": "4f56efc30d98eada3ed0",
+    "url": "/Movie-list/build/static/js/main.574c69a0.chunk.js"
+  },
+  {
+    "revision": "613f39289053b70c7274",
+    "url": "/Movie-list/build/static/js/runtime-main.e01e643b.js"
+  },
+  {
+    "revision": "3feec74bcd5abc7b0ff5300e7a5d9843",
+    "url": "/Movie-list/build/static/media/clapperBoard.3feec74b.svg"
+  },
+  {
+    "revision": "981a767244e7a00dcea64c4bcec832d3",
+    "url": "/Movie-list/build/static/media/leaguegothic-regular-webfont.981a7672.woff2"
+  },
+  {
+    "revision": "ba4cbc9ab63d19bd84022dc81cb5e034",
+    "url": "/Movie-list/build/static/media/leaguegothic-regular-webfont.ba4cbc9a.woff"
+  }
+]);
