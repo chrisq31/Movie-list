8e91d14366f7bfc135c14e38a5c3d19494b282f9
chrisquinncreative <quinn.uk@gmail.com>
2019-11-29 01:14:42 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Movie-list/build/precache-manifest.368b9f7b8f71bf8c9b63cb3e247bec05.js"
+  "/Movie-list/build/precache-manifest.92249e26ce05a87a595366f075ed27e9.js"
 );
 
 self.addEventListener('message', (event) => {
